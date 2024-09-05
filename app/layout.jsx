3bcd761d109a9471861ec2c3dcbfec287ec0681b
dpa313 <dpa.github@gmail.com>
2024-09-05 16:34:23 +0600

    page transition added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,16 @@
 // import localFont from "next/font/local";
-import {JetBrains_Mono} from "next/font/google"
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
 // components
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   variable: "--font-jetbrainsMono",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"]
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 // const geistMono = localFont({
 //   src: "./fonts/GeistMonoVF.woff",
@@ -24,11 +26,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${jetbrainsMono.variable} antialiased`}
-      >
-        <Header/>
-        {children}
+      <body className={`${jetbrainsMono.variable} antialiased`}>
+        <Header />
+        <StairTransition/>
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
